fefdc48c922e8d5f845891aca466a38cfe68addc
bsatheesh <30818500+bsatheesh@users.noreply.github.com>
2017-08-21 20:34:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var config = {
     user:'digisath',
     database:'digisath',
     password:process.env.DB_PASSWORD,
-    host:'imad.hasura.io',
+    host:'digisath.imad.hasura-app.io',
     port:'5432'
 }
 var pool= new Pool(config);
