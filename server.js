de170cf255d28da5c74b6cc17077fe5e043c1412
bsatheesh <30818500+bsatheesh@users.noreply.github.com>
2017-08-21 20:56:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articles/articlename',function(req,res))
+app.get('/articles/articlename',function(req,res)
 {
     
-}
+})
 app.get('/test-db', function (req,res)
 {
     pool.query("SELECT * FROM ARTICLE", function(err,result)
