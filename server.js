82cfd953d85f31c34e88fc863f9f18ee1f74f6cc
bsatheesh <30818500+bsatheesh@users.noreply.github.com>
2017-08-21 20:30:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/test-db', function (req,res)
     pool.query("SELECT * FROM ARTICLE", function(err,result)
     {
         if(err)
-        {res.status(500).send(err.tostring())}
+        {res.status(500).send(err.toString())}
         else
         {
             res.send(JSON.Stringify(result));
